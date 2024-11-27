65d48e4009d1c0deb89ee84f1b82a1c5aea68485
Atsujin <atsuhito1517@gmail.com>
2024-11-27 21:45:55 +0900

    ポートフォリオ改善

--- resources/views/bbs/index.blade.php
+++ resources/views/bbs/index.blade.php
@@ -5,7 +5,7 @@
         <title>皆の掲示板</title>
         @vite('resources/css/app.css')
   </head>
-  <!-- <body style="background-image:url('/storage/white_00108.jpg')"> -->
+  <body style="background-image:url('/storage/white_00108.jpg')">
   <header class="text-gray-600 body-font bg-indigo-400">
   <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
