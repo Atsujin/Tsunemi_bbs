02574633c8b5e130505f087f9c23187e5096d8df
Atsujin <atsuhito1517@gmail.com>
2024-11-28 22:05:34 +0900

    ポートフォリオ改善

--- resources/views/thread/show.blade.php
+++ resources/views/thread/show.blade.php
@@ -6,6 +6,38 @@
         @vite('resources/css/app.css')
   </head>
   <body style="background-image:url('/storage/white_00108.jpg')">
+  <header class="text-gray-600 body-font bg-indigo-400">
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+      </svg>
+      <span class="ml-3 text-xl">皆の掲示板</span>
+     </a>
+     <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <a href="/thread/create" class="mr-5 hover:text-gray-900">スレッド作成</a>
+      @auth
+      <a href="/admin" class="mr-5 hover:text-gray-900">管理人紹介</a>
+      @endauth
+    </nav>
+    @guest
+    <button onclick="location.href='/users/index'" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">ログイン
+      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24"> -->
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+    </button>
+    @endguest
+    @auth
+    <form action="{{ route('users.logout') }}" method="post">
+      @csrf
+    <button onclick="location.href='/users/index'" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">ログアウト
+      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+    </button>
+    </form>
+    @endauth
+  </div>
+</header>
   <div class="flex flex-col text-center w-full mb-20">
   <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 mt-24">
         <font style="vertical-align: inherit;">
