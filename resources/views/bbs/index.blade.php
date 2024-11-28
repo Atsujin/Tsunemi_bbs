959071e2c2bd5f338bf20ce66c1525232e7dd0bf
Atsujin <atsuhito1517@gmail.com>
2024-11-28 22:46:44 +0900

    ポートフォリオ改善

--- resources/views/bbs/index.blade.php
+++ resources/views/bbs/index.blade.php
@@ -6,13 +6,7 @@
         @vite('resources/css/app.css')
     </head>
   <body style="background-image:url('/storage/white_00108.jpg')">
-    <header class="bg-indigo-400 flex gap-3">
-      <h1>皆の掲示板</h1>
-      <ul>
-        <li><a>スレッド作成</a></li>
-      </ul>
-    </header>
-  <!-- <header class="text-gray-600 body-font bg-indigo-400">
+  <header class="text-gray-600 body-font bg-indigo-400">
   <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
@@ -27,8 +21,8 @@
     </nav>
     @guest
     <button onclick="location.href='/users/index'" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">ログイン
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24"> -->
-        <!-- <path d="M5 12h14M12 5l7 7-7 7"></path>
+      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
       </svg>
     </button>
     @endguest
@@ -43,7 +37,7 @@
     </form>
     @endauth
   </div>
-</header> -->
+</header>
 @auth
     <div class="alert alert-danger text-right w-25 mx-auto text-indigo-500 mt-4">{{ \Illuminate\Support\Facades\Auth::user()->name }}</div>
     @endauth
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ export default {
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
+        './src/**/*.{vue,js,ts,jsx,tsx}'
     ],
     theme: {
         extend: {
